ed0dbbb5830e7a2d4c4afa04f2788db279d8ae73
Christopher Caston <chris@caston.id.au>
2025-07-31 18:15:58 +0800

    adjusted limiter

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def create_app():
     limiter = Limiter(
         app=app,
         key_func=get_remote_address,
-        default_limits=["200 per day", "50 per hour"]
+        default_limits=["2000 per day", "500 per hour"]
     )
 
     # Import blueprints from the routes package. This must happen AFTER db.init_app(app)
